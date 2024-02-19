f0f915e7b747a6349e95903088e4071ec83d2d2e
sayemSS <sayenbiswas783@gmail.com>
2024-02-19 12:20:43 +0600

    sayem

--- app1/urls.py
+++ app1/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 # from app1 import views
 # from app1.views import index
 from app1.views import *
+#i will try my
 
 urlpatterns = [
     # path('', views.index, name='home')
